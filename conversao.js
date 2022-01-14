7536ec15a91e2434f0171346e0cf41e5efb5f055
marcospatton <marcospatton@hotmail.com>
2022-01-14 17:09:34 -0300

    conversao

---
+++ conversao.js
@@ -0,0 +1,14 @@
+//tipo de dado
+//booleanos
+
+//conversaõ implicita
+
+const numero = 456;
+const numeroString = '456';
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+
+//conversão explicita
