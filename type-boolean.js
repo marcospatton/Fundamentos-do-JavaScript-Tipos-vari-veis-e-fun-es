81df1ceb53f2c42266e85f8c10f4c1964f9b4185
marcospatton <marcospatton@hotmail.com>
2022-01-14 15:54:27 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,6 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
+// 5 não é igual a 10 = resposta false
---
+++ type-string.js
@@ -0,0 +1,23 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá , mundo' ;
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O leo disse "Oi!"';
+console.log(citacao)
+
+//concatenação (+)
+
+const citacao2 = "meu nome é ";
+const meuNome = "Leonardo";
+
+console.log(citacao2 + meuNome)
+
+//template string ou template literal
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
