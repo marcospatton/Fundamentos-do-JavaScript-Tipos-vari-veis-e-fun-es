5105e240622390e6e412dcec2a6d66d57c6473cb
marcospatton <marcospatton@hotmail.com>
2022-01-14 17:16:38 -0300

    subido os arquivos

--- type-boolean.js
+++ type-boolean.js
@@ -1,6 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
 const primeiroNumero = 5;
-const segundoNumero = 10;
+const segundoNumero = 5;
+let cadastroAtivado = true;
 
-console.log(primeiroNumero === segundoNumero);
+//console.log(primeiroNumero === segundoNumero);
 
-// 5 não é igual a 10 = resposta false
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
--- type-string.js
+++ type-string.js
@@ -1,23 +1,13 @@
 const texto1 = "Olá, mundo!";
-const texto2 = 'Olá , mundo' ;
+const texto2 = 'Olá, mundo!';
 const senha = "senhaSuperSegura456!";
 const StringDeNumeros = "34567";
 
-const citacao = 'O leo disse "Oi!"';
-console.log(citacao)
-
-//concatenação (+)
-
-const citacao2 = "meu nome é ";
+const citacao = "Meu nome é ";
 const meuNome = "Leonardo";
 
-console.log(citacao2 + meuNome)
+// concatenação (+)
 
-//template string ou template literal
+console.log(citacao + meuNome)
 
-const cidade = "belo horizonte";
-const input = "Belo Horizonte";
-
-const inputMinusculo = input.toLowerCase();
-
-console.log(cidade === inputMinusculo); // true
+//template string OU template literal
